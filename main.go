55a5bb0857658893c0f348a4aee0952597e2e953
Divyansh-ji <Divyanshtiwary01@gmail.com>
2025-11-29 23:45:14 +0530

    updated

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"main.go/api"
+	"main.go/db"
 	"main.go/events"
 	"main.go/notifications"
 )
 
 func main() {
+	db.LoadEnvVar()
+
+	db.ConnectTODB()
+
+	db.SyncDatabase()
+
 	bus := events.NewEventBus()
 
 	bus.Subscribe("User.Registerd", notifications.HandleUserRegistered)
 
 	notifications.UserRegisterd(bus, 101, "Divyansh", "divyanshTiwary01@gmail.com")
+
+	r := gin.Default()
+
+	r.POST("/User", api.RegisterUser)
+
+	log.Println("Server starting on :8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 
 }
